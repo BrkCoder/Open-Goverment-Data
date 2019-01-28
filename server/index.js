39c1fe646d7aaf3bbcf2a6e6028b0a9b7f821e9f
BrkCoder <baribarakinbal@gmail.com>
2019-01-28 05:38:41 +0200

    add Basic API Service + Update NodeJS Server + Add Markers on Map

--- server/index.js
+++ server/index.js
@@ -6,10 +6,33 @@ const httpPort = process.env.PORT || 8080;
 const router = require("./api");
 
 //custom middlewares
+const corsHeaders = [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Authorization'
+];
+
+const setOrigin = () => (req, res, next) => {
+    res.header(`Access-Control-Allow-Origin`, `*`);
+    res.header(`Access-Control-Allow-Methods`, `GET, PUT, POST, DELETE, OPTIONS`);
+    res.header(`Access-Control-Allow-Headers`, corsHeaders.join(`, `));
+    res.header(`Access-Control-Allow-Credentials`, `true`);
+    (`OPTIONS` === req.method) ? res.sendStatus(200) : next();
+};
+
+function setContentType() {
+    return function (req, res, next) {
+        res.contentType('application/json');
+        next();
+    };
+}
 
 
 //use custom & third-party middlewares
-
+router.use(setContentType());
+app.use(setOrigin());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(router);
