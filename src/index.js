6d556d84bc365744f26d1c52734182b102f45725
BrkCoder <baribarakinbal@gmail.com>
2019-01-30 00:47:36 +0200

    fix doublequote

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // @flow
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import 'typeface-roboto';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import "typeface-roboto";
+import App from "./App";
+import * as serviceWorker from  "./serviceWorker";
 const root = document.getElementById('root');
 if(root){
     ReactDOM.render(<App />, root);
