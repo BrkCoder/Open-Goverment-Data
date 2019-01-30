c405748ecd7bfcbe9b776c14d2b7f3004f26f288
Georgy Bunin <georgyb@mprest.com>
2019-01-30 11:37:50 +0200

    remove unused comments

--- src/gmap/GMap.js
+++ src/gmap/GMap.js
@@ -22,36 +22,4 @@ const GMap = withScriptjs(withGoogleMap((props: Props) =>
     </GoogleMap>
 ));
 
-// const styles = [
-//     {
-//         textColor: 'black',
-//         height: 53,
-//         url: m1,
-//         width: 53
-//     },
-//     {
-//         textColor: 'black',
-//         height: 56,
-//         url: m1,
-//         width: 56
-//     },
-//     {
-//         textColor: 'black',
-//         height: 66,
-//         url: m1,
-//         width: 66
-//     },
-//     {
-//         textColor: 'black',
-//         height: 78,
-//         url: m1,
-//         width: 78
-//     },
-//     {
-//         textColor: 'black',
-//         height: 90,
-//         url: m1,
-//         width: 90
-//     }];
-
 export default GMap;
